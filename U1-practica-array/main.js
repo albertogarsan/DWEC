c17e20902ced7ee3242c492bc26ffeea9e3f1df4
Alberto Garcia <albertogarsan@outlook.com>
2023-09-29 17:01:35 +0200

    Exercicis d'arrays fets

--- U1-practica-array/main.js
+++ U1-practica-array/main.js
@@ -27,17 +27,24 @@ function genRand(max){
 
 const array2 = []
 for (let i = 1; i <= 100; i++){
+    //A cada iteració dins del for s'afegix un num random que es genera a la funció superior
     array2.push(genRand(9))
 }
 
+// Creem un array de contador amb els 10 possibles posicions (0 a 9) que són els números dels quals volem portar un tracking i cada index de l'array correspon a un numero que anem afegim +1. Per no gastar 10 variables diferents, fem ús de l'array.
 let contador = [0,0,0,0,0,0,0,0,0,0]
+
+// Recorrem l'array segons la seua longitud per a passar per cada posició
 for (let i = 0; i < array2.length; i++){
+    // Amb array[i] obtenim el valor de l'array2 en la posició iteradora i guardarem eixe resultat com a valor i el passarem com a condició al switch
     const valor = array2[i]
     switch (valor){ 
         case 0: 
+            // En la posició 0 del contador si en l'array2 posicio 0 té el valor 0 entonces al contador li afegim +1
             contador[0] += 1
             break;
         case 1:
+            // Si array2 posicio 1 té el valor 1 entonces al contador li afegim +1 en la posició 1 de contador
             contador[1] += 1
             break;
         case 2:
@@ -68,6 +75,7 @@ for (let i = 0; i < array2.length; i++){
 
 }
 
+//Per últim, per cada item del contador guardem el compte i el index i ho mostrem per consola
 contador.forEach((compte, i) => {
     console.log(`El número ${i} ha aparegut ${compte.toString().padStart(2)} vegades`)
 })
@@ -82,6 +90,7 @@ for(let i = 0; i < 25; i++){
 
 function inclouNum(array){
     let num = genRand(100)
+    //Comprova si es repetix algun número, de ser que sí, torna a cridar-se la funció recursivament fins a trobar un num que no es repetisca i cumplir amb la condició del for <25 sino retorna el num i en el for s'afegix a l'array3
     if (array.includes(num)) {
         return inclouNum(array)
     } else {
@@ -90,3 +99,71 @@ function inclouNum(array){
 }
 
 console.log(array3.sort())
+
+//E5 Genera un array de 100 números aleatoris sencers entre 0 i 9. Fes que siguen 0 totes les caselles des de la 25 fins a la 75.
+const array4 = []
+
+for (let i = 0; i < 100; i++){
+    let num = genRand(9)
+    array4.push(num)
+}
+
+array4.forEach((_, index) => {
+    if (index >= 25 && index <= 75) {
+        array4[index] = 0
+    } 
+}) 
+
+/* for (let i = 0; i < array4.length; i++){
+    if (i >= 25 && i <= 75) {
+        array4[i] = 0
+    } 
+} */
+
+
+console.table(array4)
+
+//E6 Genera un vector amb noms d’animals. Demana per pantalla a l’usuari que introduisca un nom i determina si existeix al vector o no amb includes().
+/* const animals = ["Humà", "Lleó", "Pantera", "Colom"]
+const resultat = prompt("Introduix el nom d'un animal: ")
+
+if (animals.includes(resultat)){
+    alert(`Encerts!! El ${resultat}, és un animal`)
+} else {
+    alert(`El ${resultat}, no es troba a la nostra en el nostre sistema 😭`)
+} */
+
+//E7 A partir d’un vector de paraules genera un string amb totes les paraules unides.
+const paraules = ["hola", "món", "com", "va", "tot?"]
+
+console.log(paraules.join(" "))
+
+//E8 Ordena alfabèticament un vector de noms.
+const noms = ["Berto", "Antoni", "Sara", "Zacarias"]
+
+console.log(noms.sort())
+
+//E9 Fes un vector de noms. Amb filter() torna un vector que tinga aquells noms amb 6 o més lletres
+console.log(noms.filter((word) => word.length > 6))
+
+//E10 Genera una matriu on cada fila serà el nom d’un producte i el seu preu en euros. Posa el preu entre 0 i 100€. Aplica les funcions necessàries per a, a partir d’aquesta matriu, obtindre altra matriu amb aquells productes que el seu preu siga major de 75€ i s’haja aplicat una rebaixa del 10%.
+
+const productes = [
+    {article: "yoyó", preu : 2},
+    {article: "barbie", preu: 35},
+    {article: "raspberry", preu: 99},
+    {article: "guitarra", preu: 86},
+    {article: "videojoc", preu: 51},
+    ]
+
+const productesFiltrats = productes.filter((producte) => {
+    return producte.preu > 75
+})
+
+const productesFiltratsAmbRebaixa = productesFiltrats.map(producte => {
+    return producte.preu -= (producte.preu * 0.10)
+})
+
+console.log(productesFiltratsAmbRebaixa)
+
+
